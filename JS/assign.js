0a99310919c9b2d744f43355b54ba639dd3d898b
VikasDharajiya <vikasdharajiya18@gmail.com>
2025-12-09 14:07:19 +0530

    upload

---
+++ JS/assign.js
@@ -0,0 +1,22 @@
+// assignment opr : +=,-=,*=,/=,%=,**=
+let a = 10
+a += 5
+document.write('<br> A is' + a)
+a **= 2
+document.write('<br> A is' + a)
+a %= 15
+document.write('<br> A is' + a)
+
+// relational opr <,>,<=,>=,==,!=,!==,===
+
+a = 25
+b = '25'
+
+document.write('<br> a<b is' + (a < b))
+document.write('<br> a>b is' + (a > b))
+document.write('<br> a<=b is' + (a <= b))
+document.write('<br> a>=b is' + (a >= b))
+document.write('<br> a==b is' + (a == b))
+document.write('<br> a!=b is' + (a != b))
+document.write('<br> a!==b is' + (a !== b))
+document.write('<br> a===b is' + (a === b)) 
